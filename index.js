deeedb76b4672de7bb6d70cd2a91adae09615cb0
bhuwanwb <bhuwanwb@gmail.com>
2017-06-22 19:20:45 +0530

    Number PLate

--- index.js
+++ index.js
@@ -21,9 +21,6 @@ var options = { method: 'POST',
 request(options, function (error, response, body) {
   if (error) throw new Error(error);
     console.log(JSON.parse(body).response.probabilities[0][0][0]);
-    console.log(JSON.parse(body).response.probabilities[0][1][0]);
-    console.log(JSON.parse(body).response.probabilities[0][2][0]);
-     console.log(recive);
    // console.log(recive);
     var carName=JSON.parse(body).response.probabilities[0][0][0];
     
@@ -34,4 +31,36 @@ request(options, function (error, response, body) {
 });
 
     }
+    ,
+    
+    getCarNumber:function(recive, callback ){
+        
+var request = require("request");
+console.log("Base64"+recive)        
+
+var options = { method: 'POST',
+  url: 'https://api.openalpr.com/v2/recognize_bytes',
+  qs: 
+   { secret_key: 'sk_724f63a7c5a4f02f577c0371',
+     recognize_vehicle: '0',
+     country: 'gb',
+     return_image: '0',
+     topn: '10' },
+  headers: 
+   { 'postman-token': '2f612f3a-58a8-1c01-8646-d107adde9aac',
+     'cache-control': 'no-cache',
+     'content-type': 'application/json' },
+  body:recive};
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
+
+    }
 }
+
+
+
+
